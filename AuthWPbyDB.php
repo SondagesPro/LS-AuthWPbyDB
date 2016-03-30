1331f7f927e29ca4577fa67d6b06c4595bf8d7cc
Denis Chenu <denis@sondages.pro>
2016-03-30 18:34:07 +0200

    [doc] Update version - 2.50 comaptibility checked

--- AuthWPbyDB.php
+++ AuthWPbyDB.php
@@ -6,7 +6,7 @@
  * @copyright 2014 Denis Chenu <http://sondages.pro>
  * @copyright 2014 Bruce Mahillet de Komet <http://jevaluemaformation.com>
  * @license GPL v3
- * @version 1.0
+ * @version 1.1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,8 +23,8 @@ class AuthWPbyDB extends AuthPluginBase
 {
     protected $storage = 'DbStorage';
 
-    static protected $description = 'A plugin to authenticate user via WordPress DB.';
-    static protected $name = 'WordPress DB Authentification';
+    static protected $description = 'A plugin to authenticate user via WordPress DB (v1.1)';
+    static protected $name = 'AuthWPbyDB';
 
     protected $settings = array(
         'authwp_dir' => array(
